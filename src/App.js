3bf636c078af7e096f8dcb5b039e9542eea1b24e
AndrTurko <andrturko@gmail.com>
2018-04-30 22:31:23 +0300

    add HMR

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Layout className="layout">
+      <Header>
+        <div style={{color: '#fff'}}>BSUIR schedule</div>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©2016 Created by Ant UED
+      </Footer>
+    </Layout>
     );
   }
 }
